f833f9f84e850de25976c02708a128b3ef9a0808
root <root@atguigu.com>
2022-10-06 10:42:05 +0800

    first commit

---
+++ src/main/java/com/atguigu/app/HelloWorld.java
@@ -0,0 +1,12 @@
+package com.atguigu.app;
+
+/**
+ * @author shkstart
+ * @create 2022-10-06 10:35
+ */
+public class HelloWorld {
+    public static void main(String[] args) {
+
+        System.out.println("hello world! hello bigmum!");
+    }
+}
